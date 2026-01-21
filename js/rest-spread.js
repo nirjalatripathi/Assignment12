23b954a76a6211f079044831fd19f588e32f6e04
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-21 17:01:44 +0545

    [ADD]: Spread operator with arrays

--- js/rest-spread.js
+++ js/rest-spread.js
@@ -1,4 +1,19 @@
+//Rest Operator with Arrays
+
 const numbers= [5,6,7,8];
 const [first, ...rest]=numbers;
 console.log(first);
 console.log(rest);
+
+//Rest Operator in Functions
+function countArguments(...args) {
+    return args.length;
+}
+console.log ("Total arguments:", countArguments(2,4,5,6));
+
+//Spread Operator with Arrays
+const arr1=[1,2,3];
+const arr2=[4,5,6];
+const merged=[...arr1, ...arr2];
+console.log(merged);
+
