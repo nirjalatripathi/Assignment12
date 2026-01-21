9f9a5919ebb7512bcb668f4e1ca359bf24f77a68
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-21 17:53:55 +0545

    [ADD]: concat()

--- js/non-mutating.js
+++ js/non-mutating.js
@@ -5,3 +5,11 @@ let slicedArray = numbersArray.slice(1, 4);
 
 console.log("Original:", numbersArray);
 console.log("Sliced:", slicedArray);
+
+//Q.2. concat()
+const a=[4,5];
+const b=[6,7];
+const combined=a.concat(b);
+console.log("Value of A: ", a);
+console.log("Value of B:", b);
+console.log ("Combined values:", combined);
