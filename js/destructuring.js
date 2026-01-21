9f65cfbf3eefdb2b8209b03b5b77646199981c95
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-21 11:58:53 +0545

    [ADD]: Array Destructuring

---
+++ js/destructuring.js
@@ -0,0 +1,11 @@
+//Q.1 Array Destructuring
+//Create an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+//Destruct first two values into seperate variables
+const [first, second]= numbers;
+//Print
+console.log("First value:", first);
+console.log("second value:", second);
+
+
+
