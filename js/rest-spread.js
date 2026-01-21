e18aaba3bb77f214dc8b9597f718ca7c006a27f9
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-21 17:28:17 +0545

    [ADD]: Spread Operator with objects

--- js/rest-spread.js
+++ js/rest-spread.js
@@ -1,26 +1,35 @@
-//Rest Operator with Arrays
-
+//Q.1.Rest Operator with Arrays
 const numbers= [5,6,7,8];
 const [first, ...rest]=numbers;
 console.log(first);
 console.log(rest);
 
-//Rest Operator in Functions
+//Q.2.Rest Operator in Functions
 function countArguments(...args) {
     return args.length;
 }
 console.log ("Total arguments:", countArguments(2,4,5,6));
 
-//Spread Operator with Arrays
+//Q.3.Spread Operator with Arrays
 const arr1=[1,2,3];
 const arr2=[4,5,6];
 const merged=[...arr1, ...arr2];
 console.log(merged);
 
-// Copying an Array
+//Q.4. Copying an Array
 const originalArray= [5,6,7];
 let copiedArray=[...originalArray];
 copiedArray.push (8);
 console.log (originalArray);
 console.log(copiedArray);
 
+//Q.5. Spread Operator with Objects
+let properties={
+    name:"Nirjala",
+    age: 19
+};
+let newProperties={
+    ...properties,
+    Faculty:"CSIT"
+};
+console.log(newProperties);
